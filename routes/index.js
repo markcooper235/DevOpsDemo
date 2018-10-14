39b504f2185937630fbf0f9c20573cd992f9051d
Mark E. Cooper <mark.cooper235@gmail.com>
2018-10-14 16:28:05 -0400

    Updated the route, and view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var exec = require("child_process").exec;
+var start_time = new Date();
+
+const ip_display_command = "cat ./access.log |awk '{print $1}' |sort |uniq -c |sort -n |tail";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+   var time = process.uptime();
+   var uptime = format(time);
+   exec(ip_display_command, function (err, stdout, stderr) { 
+   if (err) handleError();
+   res.render('index', { title: 'My IP List App',
+                         start_time: start_time,
+                         uptime: uptime,
+                         ip_command_results: stdout});
+   }); 
 });
 
 module.exports = router;
+
+function format(seconds){
+	          function pad(s){
+			       return (s < 10 ? '0' : '') + s;
+			   }
+	          var hours = Math.floor(seconds / (60*60));
+	          var minutes = Math.floor(seconds % (60*60) / 60);
+	          var seconds = Math.floor(seconds % 60);
+
+	          return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+}
